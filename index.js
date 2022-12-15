c1a786c47b85740836af91bd206c47d6e76fd698
Carlos Diez <100435064+Carlosdi1106@users.noreply.github.com>
2022-12-15 10:22:44 +0000

    Cambios

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const session = require('express-session')
 const express = require("express");
+const mongojs = require('mongojs')
+const db = mongojs('mongodb://127.0.0.1:27017/test', ['login'])
 const app = express()
 
 const PORT = 4000;
 
+let indexRoute = require("./routes/index.js")
 // use static files
 app.use(express.static("public"));
 
@@ -21,20 +24,41 @@ const sess = {
 }
 app.use(session(sess))
 
+app.use('/', indexRoute)
 //username and password
 const myusername = 'user1'
 const mypassword = 'mypassword'
 
-app.get('/protected',(req,res) => {
+/*app.get('/protected',(req,res) => {
 
     if(req.session.userid){
         res.send("Welcome User <a href=\'/logout'>click to logout</a>");
     }else
         res.redirect('form.html')
-});
+});*/
 
 app.post('/user',(req,res) => {
-    if(req.body.username == myusername && req.body.password == mypassword){
+    db.login.find({"username": req.body.username, "password": req.body.password},(err,result)=>{
+        if(err){
+            console.log(err)
+        }else{
+            if(Object.keys(result).length !== 0) {
+                document.getElementById("Error").style.visibility='hidden'
+                console.log("CORRECTO")
+                req.session.userid = req.body.username;
+                console.log(req.session)
+                res.redirect('/protected');
+            }else{
+                document.getElementById("Error").style.visibility='visible'
+                console.log("MAL")
+
+            }
+        }
+    })
+
+
+
+    /*if(req.body.username == myusername && req.body.password == mypassword){
 
         req.session.userid=req.body.username;
         console.log(req.session)
@@ -42,13 +66,14 @@ app.post('/user',(req,res) => {
     }
     else{
         res.send('Invalid username or password');
-    }
+    }*/
 })
 
-app.get('/logout',(req,res) => {
+
+/*app.get('/logout',(req,res) => {
     req.session.destroy();
     res.redirect('/');
-});
+});*/
 
 app.get("/", (req, res) => {
     res.send("Hello World");
